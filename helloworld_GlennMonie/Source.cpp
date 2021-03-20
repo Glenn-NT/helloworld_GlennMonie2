9f692e8e3330a6985979042ef215a2bed48c9070
glennM <glennmoonie001@gmail.con>
2021-03-20 14:08:17 -0400

    first commit

---
+++ helloworld_GlennMonie/Source.cpp
@@ -0,0 +1,19 @@
+//Name: Glenn Monie
+//Date: March 20 2021
+//Project Name: HelloWorld
+//Project Description: Print Hello World
+
+//Libraries
+#include<iostream>
+
+//Project structure
+using namespace std;
+
+int main() {
+
+	cout << " Hello World " << endl;
+
+	cout << endl;
+	system("pause");
+	return 0;
+}
